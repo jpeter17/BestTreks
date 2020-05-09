5d62090a7e4b0a3b2d198a8e2dd9e007d42debef
Jake Petersen <jakepetersen1221@gmail.com>
2020-05-08 20:11:30 -0700

    Rudimentary trail slugs implemented

--- app.py
+++ app.py
@@ -13,15 +13,30 @@ db.Model.metadata.reflect(db.engine)
 class Trail(db.Model):
     __tablename__ = 'Trails'
     __table_args__ = { 'extend_existing': True }
-    LOC_CODE = db.Column(db.Text, primary_key=True)
+    TRAIL_ID = db.Column(db.Text, primary_key=True)
 
 @app.route('/')
 def hello():
-    return render_template("index.html")
+    # ORM Practice
+    """ 
+    print("Total number of trails is", Trail.query.count())
 
-@app.route('/shoelaces')
-def shoelaces():
-    return "This works now!"
+    trail = Trail.query.filter_by(TRAIL_ID='240 -34').first()
+    print("Trail's name is", trail.PMA_NAME)
+
+    """
+    
+    trailCount = Trail.query.count()
+
+    good_trails = Trail.query.filter_by(CONDITION='Good').all()
+
+    return render_template("index.html", count = trailCount, trails=good_trails)
+
+@app.route('/trails/<slug>')
+def detail(slug):
+    trail = Trail.query.filter_by(TRAIL_ID=slug).first()
+
+    return trail.PMA_NAME
 
 if __name__=='__main__':
     app.run(debug=True)
